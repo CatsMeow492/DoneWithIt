775b8a27869a929b0255be8d3c388b14fa83bd53
CatsMeow492 <calisurf243@hotmail.com>
2021-12-22 15:25:28 -0800

    welcome screen

--- App.js
+++ App.js
@@ -1,19 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, SafeAreaView, Image, TouchableWithoutFeedback } from 'react-native';
+import WelcomeScreen from './app/screens/WelcomeScreen'
 
 export default function App() {
   const handlePress = () => {console.log("Pressed")}
   return (
-    <SafeAreaView style={styles.container}>
-      <Text onPress={handlePress}>Hey Bud</Text>
-      <TouchableWithoutFeedback onPress={ () => {console.log("Dont touch that!")}}>
-      <Image source={{
-        width: 200,
-        height: 300,
-        uri: "https://picsum.photos/200/300",
-      }} />
-    </TouchableWithoutFeedback><StatusBar style="auto" />
-    </SafeAreaView>
+    <WelcomeScreen />
   );
 }
 
