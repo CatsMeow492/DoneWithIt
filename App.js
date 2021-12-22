432ba04942ec3f2bc131d06b639480c74a581ff8
CatsMeow492 <calisurf243@hotmail.com>
2021-12-22 15:55:48 -0800

    adding text to login/signup buttons

--- App.js
+++ App.js
@@ -3,17 +3,9 @@ import { StyleSheet, Text, View, SafeAreaView, Image, TouchableWithoutFeedback }
 import WelcomeScreen from './app/screens/WelcomeScreen'
 
 export default function App() {
-  const handlePress = () => {console.log("Pressed")}
   return (
     <WelcomeScreen />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create({});
--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import { ImageBackground, StyleSheet } from 'react-native'
+import { ImageBackground, StyleSheet, View, Image, Text } from 'react-native'
 
 function WelcomeScreen(props) {
     return (
-        <ImageBackground source={require('../assets/background.jpg')}>
+        <ImageBackground style={styles.background} source={require('../assets/background.jpg')}>
+            <Image style={styles.logo} source={require('../assets/logo-red.png')} />
+            <View style={styles.loginButton}>
+                <Text style={styles.loginText}>Login</Text>
+            </View>
+            <View style={styles.registerButton}></View>
         </ImageBackground>
     );
 }
@@ -11,6 +16,31 @@ function WelcomeScreen(props) {
 const styles = StyleSheet.create({
     background: {
         flex: 1,
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+    },
+    loginButton: {
+        width: '100%',
+        height: 70,
+        backgroundColor: "#fc5c65"
+    },
+    loginText: {
+        color: 'white',
+        fontSize: 'Roboto',
+        justifyContent: 'center'
+        alignItems: 'center',
+    },
+    registerButton: {
+        width: '100%',
+        height: 70,
+        backgroundColor: "#4ecdc4"
+    },
+    logo: {
+        position: 'absolute',
+        width: 100,
+        height: 100,
+        top: 70,
+        justifyContent: 'center',
     }
 })
 
