eb4bc9be6265f78670824adaedd18fb1c6ea6aaf
CatsMeow492 <calisurf243@hotmail.com>
2021-12-22 11:26:57 -0800

    still adding assets

--- App.js
+++ App.js
@@ -1,17 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Image, TouchableWithoutFeedback } from 'react-native';
 
 export default function App() {
   const handlePress = () => {console.log("Pressed")}
   return (
     <SafeAreaView style={styles.container}>
       <Text onPress={handlePress}>Hey Bud</Text>
+      <TouchableWithoutFeedback onPress={ () => {console.log("Dont touch that!")}}>
       <Image source={{
         width: 200,
         height: 300,
         uri: "https://picsum.photos/200/300",
       }} />
-      <StatusBar style="auto" />
+    </TouchableWithoutFeedback><StatusBar style="auto" />
     </SafeAreaView>
   );
 }
